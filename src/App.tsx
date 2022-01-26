71e1c2019a09f415128f5568c9e91c32a2708f18
Valerii Radchenko <valerii.radchenko@freshcodeit.com>
2022-01-26 12:36:50 +0200

    Add state for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form/Form';
 import NavBar from './components/NavBar/NavBar';
+import { ITodosState } from './types/types';
 
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState<ITodosState[]>([]);
+
+  const addHandler = (task: string) => {
+    const newEntry = {
+      task: task,
+      id: Date.now(),
+      isComplited: false
+    }
+    setTodos([newEntry, ...todos]);
+  }
+
   return (
     <>
       <NavBar/>
-      <Form />
+      <Form addTask={addHandler}/>
     </>
   );
 }
