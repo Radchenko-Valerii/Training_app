17ff05cbc95267f5c3eb0bb63729c5396dec901c
Valerii Radchenko <valerii.radchenko@freshcodeit.com>
2022-01-26 16:10:38 +0200

    Add types & interfaces

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,26 @@ export interface ITodosState{
   isComplited: boolean,
   id: number
 }
+
+export type ITodos = {
+  tasks: ITodosState[],
+  doneHandler: (id: number) => any,
+  removeHandler: (id: number) => any
+}
+
+export type ITodo = {
+  doneHandler: (id: number) => any,
+  removeHandler: (id: number) => any,
+  task: string,
+  isComplited: boolean,
+  id: number
+}
+
+export interface IModal {
+  text: string,
+  showing: boolean
+}
+
+// export interface IModalState{
+//   showing: boolean
+// }
