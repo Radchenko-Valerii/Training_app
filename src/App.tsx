29752d795c4ff2c01b34ec3802233056ea32fb5d
Valerii Radchenko <valerii.radchenko@freshcodeit.com>
2022-01-26 16:09:18 +0200

    Add logic to todo list & modal window

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import Form from './components/Form/Form';
+import ModalWindow from './components/ModalWindow/ModalWindow';
 import NavBar from './components/NavBar/NavBar';
+import ToDoList from './components/ToDoList/ToDoList';
 import { ITodosState } from './types/types';
 
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodosState[]>([]);
+  const [showing, setShowing] = useState<boolean>(false);
 
   const addHandler = (task: string) => {
     const newEntry = {
@@ -16,10 +19,24 @@ const App: React.FC = () => {
     setTodos([newEntry, ...todos]);
   }
 
+  const doneHandler = (id: number) => {
+    setTodos(prev => prev.map((task) => {
+      if(task.id === id){
+        task.isComplited = !task.isComplited
+    } return task}))
+  }
+
+  const removeHandler = (id: number) => {
+    setShowing(true);
+    setTodos(prev => prev.filter((task)=> task.id!==id))
+  }
+
   return (
     <>
       <NavBar/>
       <Form addTask={addHandler}/>
+      <ToDoList tasks={todos} doneHandler={doneHandler} removeHandler={removeHandler}/>
+      <ModalWindow text={'Are you shure?'} showing={showing}/>
     </>
   );
 }
